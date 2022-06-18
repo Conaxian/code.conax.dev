1dc5a5957eb119918021bd680679f51cfa09b3dc
Conaxian <conax@conax.cz>
2022-06-18 23:51:49 +0200

    Add multiple file support to editor

--- src/scripts/editor.ts
+++ src/scripts/editor.ts
@@ -1,4 +1,12 @@
+/**
+ *
+ * Code Editor
+ *
+ */
+
 import { executePython } from "./api.js";
+import type VirtualFileSystem from "./vfs.js";
+import type { VirtualFile } from "./vfs.js";
 
 interface EditorElementIdMap {
   editor: string;
@@ -7,13 +15,19 @@ interface EditorElementIdMap {
 }
 
 class Editor {
+  private readonly vfs: VirtualFileSystem;
+  private currentFile: VirtualFile;
+
   private readonly editorElem: HTMLTextAreaElement;
   private readonly executeElem: HTMLButtonElement;
   private readonly outputElem: HTMLTextAreaElement;
 
   private text: string;
 
-  constructor(elementIds: EditorElementIdMap) {
+  constructor(vfs: VirtualFileSystem, elementIds: EditorElementIdMap) {
+    this.vfs = vfs;
+    this.currentFile = vfs.files[0];
+
     this.editorElem = document.getElementById(
       elementIds.editor
     ) as HTMLTextAreaElement;
@@ -28,19 +42,27 @@ class Editor {
     this.outputElem.value = "";
     this.text = "";
 
+    this.fileSwitchHandler();
+
     const editorInputHandler = this.editorInputHandler.bind(this);
     this.editorElem.addEventListener("input", editorInputHandler);
 
     const executeClickHandler = this.executeClickHandler.bind(this);
     this.executeElem.addEventListener("click", executeClickHandler);
+
+    const fileSwitchHandler = this.fileSwitchHandler.bind(this);
+    window.addEventListener("hashchange", fileSwitchHandler);
+
+    const ignoreCtrlS = this.ignoreCtrlS.bind(this);
+    window.addEventListener("keydown", ignoreCtrlS);
   }
 
   private editorInputHandler() {
-    this.text = this.editorElem.value;
+    this.currentFile.content = this.editorElem.value;
   }
 
   private async executeClickHandler() {
-    const result = await executePython(this.text);
+    const result = await executePython(this.vfs);
 
     this.outputElem.value = result.output;
 
@@ -48,6 +70,22 @@ class Editor {
       this.outputElem.classList.remove("error");
     } else {
       this.outputElem.classList.add("error");
+    }
+  }
+
+  private fileSwitchHandler() {
+    const name = window.location.hash.slice(1);
+    const file = this.vfs.getFile(name);
+
+    if (file) {
+      this.currentFile = file;
+      this.editorElem.value = file.content;
+    }
+  }
+
+  private ignoreCtrlS(event: KeyboardEvent) {
+    if (event.ctrlKey && event.key === "s") {
+      event.preventDefault();
     }
   }
 }
