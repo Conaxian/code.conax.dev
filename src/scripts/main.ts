4f1b831e9784cb9a2e8802ce02adc5995f9db473
Conaxian <conax@conax.cz>
2022-06-19 13:34:25 +0200

    Refactor main module

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,10 +1,39 @@
-import Editor from "./editor.js";
-import VirtualFileSystem from "./vfs.js";
+import Kernel from "./kernel.js";
+import Editor from "./components/editor.js";
+import Terminal from "./components/terminal.js";
+import VirtualFileSystem from "./components/vfs.js";
+import FileBar from "./components/filebar.js";
+import Executor from "./components/executor.js";
+import Controls from "./components/controls.js";
 
-const vfs = new VirtualFileSystem({ topBar: "topbar", newButton: "new" });
+const INITIAL_FILE_NAME = "main.py";
 
-const editor = new Editor(vfs, {
-  editor: "editor",
-  execute: "execute",
-  output: "output",
+const vfs = new VirtualFileSystem();
+
+const executor = new Executor();
+
+const editor = new Editor({
+  editorElemId: "editor",
+  currentFile: vfs.files[0],
 });
+
+const terminal = new Terminal({ terminalElemId: "terminal" });
+
+const fileBar = new FileBar({
+  fileBarElemId: "filebar",
+  newButtonElemId: "new",
+});
+
+const controls = new Controls({ executeElemId: "execute" });
+
+const kernel = new Kernel({
+  editor,
+  terminal,
+  vfs,
+  fileBar,
+  executor,
+  controls,
+});
+
+const file = kernel.createFile({ name: INITIAL_FILE_NAME });
+editor.setCurrentFile(file);
